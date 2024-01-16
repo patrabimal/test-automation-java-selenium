62f79e5121b092921a066b5e86ceea4967c5caf8
Bimal Patra <bimalpatra03@gmail.com>
2024-01-16 11:38:07 +0530

    addressed feedback comment

--- src/test/java/com/qa/restassured/GetResponse.java
+++ src/test/java/com/qa/restassured/GetResponse.java
@@ -1,21 +1,21 @@
 package com.qa.restassured;
 
 import io.restassured.response.Response;
+import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.given;
 
 public class GetResponse {
-    @org.testng.annotations.Test
+    @Test
     public void extractResponse() {
         Response response = given().
                 baseUri("https://restful-booker.herokuapp.com").
-                header("x-api-key", "PMAK-659d4181012352002a6c4278-ec8e0bbf0d1f186092ed3ddf6dac9ecae0").
+                header("x-api-key", "PMAK-65a61915fb1cc57ce33451e6-f381c9710bd9ad1950681626aec7a728e7").
                 when().
-                get("/booking/4083").
+                get("/booking/3074").
                 then().
                 log().all().
                 assertThat().statusCode(200).
                 extract().response();
-        System.out.println("Response = " + response.asString());
     }
 }
--- src/test/java/com/qa/restassured/VerifyNameWithStaticImports.java
+++ src/test/java/com/qa/restassured/VerifyNameWithStaticImports.java
@@ -1,11 +1,13 @@
 package com.qa.restassured;
 
+import org.testng.annotations.Test;
+
 import static io.restassured.RestAssured.*;
 
 import static org.hamcrest.Matchers.*;
 
 public class VerifyNameWithStaticImports {
-
+    @Test
     public void shouldMatchFirstAndLastName() {
         given().
                 baseUri("https://restful-booker.herokuapp.com").header("x-api-key", "PMAK-659d4181012352002a6c4278-ec8e0bbf0d1f186092ed3ddf6dac9ecae0").
