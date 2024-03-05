63c4e21f63741b33fdc6881ed8225a94527b676c
Bimal Patra <bimalpatra03@gmail.com>
2024-03-05 10:39:44 +0530

    Add script for logging

--- src/test/java/com/qa/restassured/GetStatus.java
+++ src/test/java/com/qa/restassured/GetStatus.java
@@ -7,13 +7,14 @@ import static io.restassured.RestAssured.given;
 public class GetStatus {
     @org.testng.annotations.Test
     public void validateStatusCode() {
-        given().
-                baseUri("https://restful-booker.herokuapp.com").
-                header("x-api-key", "PMAK-659d4181012352002a6c4278-ec8e0bbf0d1f186092ed3ddf6dac9ecae0").
-                when().
-                get("/booking/342").
-                then().
-                log().all(). //output entire response in console
-                assertThat().statusCode(200);
+        given()
+        .baseUri("https://restful-booker.herokuapp.com")
+        .header("x-api-key", "PMAK-659d4181012352002a6c4278-ec8e0bbf0d1f186092ed3ddf6dac9ecae0")
+        .when()
+        .get("/booking/342")
+        .then()
+        .log().all() //output entire response in console
+        .assertThat()
+        .statusCode(200);
     }
 }
